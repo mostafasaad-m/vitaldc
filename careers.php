a6a926e115cdcb50a697aa550fd81b80dd3bb258
mostafasaad-m <20170301058@dogus.edu.tr>
2026-07-28 05:04:13 +0300

    initial project

--- careers.php
+++ careers.php
@@ -1,14 +1,16 @@
 <?php 
 /**
  * Template Name: careers
- */
+* 
+*/
 
  ?>
  <?php get_header(); ?>
 
 <style>
         .material-symbols-outlined {
-            font-variation-settings: 'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 24;
+           
+        font-variation-settings: 'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 24;
         }
         .glass-card {
             background: rgba(255, 255, 255, 0.02);
